f0f81a61827fa94a7307ec1f24384707360d0c73
Jason Long <jason@blackantmedia.com>
2009-12-18 10:23:45 -0500

    markup cleanup

--- public/docs/js/bam.js
+++ public/docs/js/bam.js
@@ -9,6 +9,7 @@ function progressiveEnhancements()
 	
 	// wireframe guidelines
   $('<div class="guideline"></div>').prependTo('#colophon').css({'height':'1px', 'width':'100%', 'position':'relative', 'top':'17px'});
+  
   $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-1px', 'position':'absolute'});  
   $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-20px', 'position':'absolute'});  
   $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-200px', 'position':'absolute'});  
