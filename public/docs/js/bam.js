c4421afe8cf2cd34ed3ca45c732f6a2cb800e929
Jason Long <jason@blackantmedia.com>
2009-12-18 09:55:34 -0500

    added wireframe colophon w/ guidelines

--- public/docs/js/bam.js
+++ public/docs/js/bam.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
 
 function progressiveEnhancements()
 {
-	$('body').prepend('<div id="highlight"></div>');
+  // top lighting effect
+	$('body').prepend('<div id="spotlight"></div>');
+	
+	// wireframe guidelines
+  $('<div class="guideline"></div>').prependTo('#colophon').css({'height':'1px', 'width':'100%', 'position':'relative', 'top':'17px'});
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-1px', 'position':'absolute'});  
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-20px', 'position':'absolute'});  
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'-200px', 'position':'absolute'});  
+  
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'600px', 'position':'absolute'});  
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'620px', 'position':'absolute'});  
+  $('<div class="guideline"></div>').prependTo('#colophon .inner').css({'height': $('#colophon').height(), 'width':'1px', 'margin-top':'-15px', 'margin-left':'820px', 'position':'absolute'});  
 }
