bf0ab4e5f0eaf4d0eb10a6446020dc61028f8a32
Jason Long <jason@blackantmedia.com>
2010-01-14 15:05:14 -0500

    added production and staging deploy recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,24 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 
+set :application,       "blackantmedia"
 set :use_sudo,          false
 set :home_path,         "/var/www/mysitename"
 set :scm,               :git
 set :repository,        "git@github.com:blackant/blackantmedia.git"
-set :branch,            "master"
+set(:current_branch)    { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
+set :branch,            defer { current_branch }
 set :repository_cache,  "git_cache"
 set :deploy_via,        :remote_cache
-set :deploy_to,         "/home/blackant/subdomains/v2"
 role :web,              "blackant"
+
+task :production do
+  set :deploy_to,         "/home/blackant/blackantmedia"
+end
+
+task :staging do
+  set :deploy_to,         "/home/blackant/subdomains/v2"
+end
 
 # the restart task apparently needs to be here, let's use 
 # it to run the cleanup task (keep last 5 releases)
