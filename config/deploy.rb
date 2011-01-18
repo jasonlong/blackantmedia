4b96b165aec3fc182dd3164b6f388bfd8904953d
Jason Long <jason@blackantmedia.com>
2011-01-17 19:32:08 -0500

    updating capistrano script

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,16 +5,25 @@ set :use_sudo,          false
 set :home_path,         "/var/www/mysitename"
 set :scm,               :git
 set :repository,        "git@github.com:blackant/blackantmedia.git"
-set :branch,            "master"
+set(:current_branch)     { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
+set :branch,             defer { current_branch }
 set :repository_cache,  "git_cache"
 set :deploy_via,        :remote_cache
 set :deploy_to,         "/home/blackant/subdomains/v2"
 role :web,              "blackant"
 
-# the restart task apparently needs to be here, let's use 
-# it to run the cleanup task (keep last 5 releases)
+
 namespace :deploy do
   task :restart do
+    symlink_settings
     cleanup
   end
 end
+
+task :symlink_settings do
+ run <<-CMD
+   rm -rf #{release_path}/config/settings.php &&
+   ln -nfs #{shared_path}/settings.php #{release_path}/config/settings.php
+ CMD
+end
+
