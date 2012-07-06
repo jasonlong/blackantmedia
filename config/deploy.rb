31f2cfa566a4e123afd3c0f73e93a92ea8a62fde
Jason Long <jason@blackantmedia.com>
2012-07-05 20:23:49 -0400

    updated github urls

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 set :use_sudo,          false
 #set :home_path,         "/var/www/mysitename"
 set :scm,               :git
-set :repository,        "git@github.com:blackant/blackantmedia.git"
+set :repository,        "git@github.com:jasonlong/blackantmedia.git"
 set(:current_branch)     { `git branch`.match(/\* (\S+)\s/m)[1] || raise("Couldn't determine current branch") }
 set :branch,             defer { current_branch }
 set :repository_cache,  "git_cache"
